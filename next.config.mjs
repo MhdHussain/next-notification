ae766b2426f7137dc3f4431ea2b8a2f688b4720c
Mohammed Hussein <mhdhussein@outlook.com>
2025-02-19 23:15:52 +0300

    fixed _next

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,10 @@
 const nextConfig = {
     output: 'export',
     distDir: 'docs',
+    basePath: '/next-notification',
+    images: {
+        unoptimized: true, // Required for GitHub Pages to serve images correctly
+      },
 };
 
 export default nextConfig;
